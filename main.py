cc2c455c12a83bbc89d33e8e86ca9974c22349bc
Tobias Waslowski <tobias.waslowski@otto-payments.de>
2024-07-31 15:05:39 +0200

    add publish function

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
+import boto3
+import click
 import datetime
 import json
 import os
 import time
-
-import boto3
-import click
 from click import secho
 
 """
 Script for creating and analyzing SQS messages.
 """
 
-QUEUE_URL = os.environ["QUEUE_URL"]
+QUEUE_URL = os.environ.get("QUEUE_URL")
+SNS_TOPIC_ARN = os.environ["SNS_TOPIC_ARN"]
 OUT_FILE = "sqs_messages.jsonl"
 POLLING_FREQUENCY = 5
 
-client = boto3.client("sqs", region_name="eu-central-1")
+sqs_client = boto3.client("sqs", region_name="eu-central-1")
+sns_client = boto3.client("sns", region_name="eu-central-1")
 
 
 @click.group()
@@ -24,10 +25,14 @@ def cli():
 
 
 @click.command()
-def receive():
+@click.option("--delete", is_flag=True, default=True, help="The URL of the SQS queue")
+@click.option("--full-message", is_flag=True, default=False, help="Store the full received SNS message")
+@click.option("--queue-url", default=QUEUE_URL, help="Queue URL; can be overwritten by environment variable")
+def receive(delete, full_message, queue_url):
     while True:
-        messages = client.receive_message(
-            QueueUrl=QUEUE_URL, MaxNumberOfMessages=10, VisibilityTimeout=1200
+        secho(f"{datetime.datetime.now().isoformat()}: Polling for messages on Queue {queue_url}")
+        messages = sqs_client.receive_message(
+            QueueUrl=queue_url, MaxNumberOfMessages=10, VisibilityTimeout=1200
         ).get("Messages", [])
         if not messages:
             secho(f"{datetime.datetime.now().isoformat()}: No messages found")
@@ -36,30 +41,44 @@ def receive():
         else:
             secho(f"Found {len(messages)} messages")
             for message in messages:
-                sns_message = json.loads(message["Body"])["Message"]
-                alerts = json.loads(sns_message)["alerts"]
+                body = json.loads(message["Body"])["Message"]
+                alerts = json.loads(body)["alerts"]
                 for alert in alerts:
                     secho(
                         f"{alert['startsAt'].split('+')[0].strip().replace(' ', 'T')} - {alert['labels']['service']} - {alert['labels']['alertname']}"
                     )
                 with open(OUT_FILE, "a") as f:
-                    f.write(json.dumps(alerts) + "\n")
-                client.delete_message(
-                    QueueUrl=QUEUE_URL, ReceiptHandle=message["ReceiptHandle"]
-                )
+                    if full_message:
+                        f.write(json.dumps(message) + "\n")
+                    else:
+                        f.write(json.dumps(alerts) + "\n")
+                if delete:
+                    sqs_client.delete_message(
+                        QueueUrl=QUEUE_URL, ReceiptHandle=message["ReceiptHandle"]
+                    )
                 secho("Message deleted")
         secho(f"Waiting {POLLING_FREQUENCY} seconds")
         time.sleep(POLLING_FREQUENCY)
 
 
 @click.command()
+@click.option("--event", required=True, help="Message to publish; reads from a JSON file")
+def publish(event):
+    with open(event, "r") as f:
+        message = json.load(f)
+    sns_client.publish(TopicArn=SNS_TOPIC_ARN, Message=json.dumps(message))
+    secho("Message published")
+
+
+@click.command()
 def purge():
-    client.purge_queue(QueueUrl=QUEUE_URL)
-    secho("Queue purged")
+    sqs_client.purge_queue(QueueUrl=QUEUE_URL)
+    secho(f"Queue {QUEUE_URL} purged")
 
 
 cli.add_command(purge)
 cli.add_command(receive)
+cli.add_command(publish)
 
 if __name__ == "__main__":
     cli()
